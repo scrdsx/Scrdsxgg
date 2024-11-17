83728ba424b75fd5077329a75af9b0daffd26c3d
scrdsx <138362343+scrdsx@users.noreply.github.com>
2024-11-17 13:08:56 +0100

    Update Source5.lua

--- Source5.lua
+++ Source5.lua
@@ -1,7 +1,3 @@
-script_key = "test"  -- Set your script key value here, if needed
+script_key = "test"
 
--- Load the script from the given URL and execute it
-loadstring(game:HttpGet("https://raw.githubusercontent.com/scrdsx/Scrdsxgg/refs/heads/main/Source5.lua", true))()
-
--- Your own script code (for example, printing "Hello World")
 print("Hello World")
