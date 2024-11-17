ea435c854ee484620889ee2f9cca651c17945b2e
scrdsx <138362343+scrdsx@users.noreply.github.com>
2024-11-17 22:53:03 +0100

    Update Source5.lua

--- Source5.lua
+++ Source5.lua
@@ -1,6 +1,6 @@
 -- The script requires the key to be defined before execution.
 -- You MUST set `script_key` to the correct value above this line.
-local script_key = ""  -- <-- Put the correct key here, e.g., script_key = "test"
+local script_key = "test"  -- <-- Put the correct key here, e.g., script_key = "test"
 
 -- The correct key that will unlock the execution
 local correct_key = "test"  -- <-- The actual correct key
@@ -11,6 +11,8 @@ if script_key == correct_key then
     local remote_script = game:HttpGet("https://raw.githubusercontent.com/scrdsx/Scrdsxgg/refs/heads/main/Source5.lua", true)
     loadstring(remote_script)()
 else
+
+print("Done")
     -- If the key is incorrect or not set, prevent the script from running
     print("Error: Incorrect or missing key. Script not executed.")
 end
